88ba6a74febcc289177538bad599c3dab02999a9
shakhob <shakhob.tursunov@gmail.com>
2021-09-19 18:02:33 +0500

    Yangilanish!

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Hello EBO Media"),
+        child: Text("Hello World"),
       )
     );
   }
